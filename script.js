3ea9cb3c9893eec4a7fc0ae5662622599c1d97aa
Manish Budhathoki <64700505+mbudhathoki200@users.noreply.github.com>
2023-07-03 06:44:29 +0545

    ✅Fixed Error Msg

--- script.js
+++ script.js
@@ -2,35 +2,120 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-
-///////////////////////////////////////
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    const html = `
-            <article class="country">
-            <img class="country__img" src="${data.flags.svg}" />
-            <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${Number(
-              data.population / 1000000
-            ).toFixed(1)} people</p>
-                <p class="country__row"><span>🗣️</span>${data.languages.nep}</p>
-                <p class="country__row"><span>💰</span>${
-                  data.currencies.NPR.name
-                }</p>
-                  </div>
-                  </article>
-                  `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  });
+const renderCountry = function (data, className = '') {
+  const html = `
+              <article class="country ${className}">
+              <img class="country__img" src="${data.flags.svg}" />
+              <div class="country__data">
+              <h3 class="country__name">${data.name.common}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${Number(
+                data.population / 1000000
+              ).toFixed(1)} people</p>
+                  <p class="country__row"><span>🗣️</span>${data.continents}</p>
+                  <p class="country__row"><span>💰</span>${data.capital}</p>
+                    </div>
+                    </article>
+                    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
 };
-// getCountryData('portugal');
-getCountryData('nepal');
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+///////////////////////////////////////
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     const html = `
+//             <article class="country">
+//             <img class="country__img" src="${data.flags.svg}" />
+//             <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${Number(
+//               data.population / 1000000
+//             ).toFixed(1)} people</p>
+//                 <p class="country__row"><span>🗣️</span>${data.continents}</p>
+//                 <p class="country__row"><span>💰</span>${data.capital}</p>
+//                   </div>
+//                   </article>
+//                   `;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// const getCountryAndNeighbour = function (country) {
+//   //AJAX call Country I
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+//   console.log(request.responseText);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     //Render Country I
+//     renderCountry(data);
+//     //Get neighbouring country II
+//     const neighbour = data.borders?.[0];
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     request2.send();
+//     request2.addEventListener('load', function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+// getCountryAndNeighbour('nepal');
+// // getCountryData('portugal');
+// // getCountryData('nepal');
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+const request = fetch('https://restcountries.com/v3.1/name/nepal');
+console.log(request);
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+// getCountryData('nepal');
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      console.log(neighbour);
+      //?Neighbouring country
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'))
+    .catch(err => {
+      console.log(`${err}💣💣💣`);
+      renderError(`Something Went Wrong 💣💣💣 ${err.message}, Try Again!!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+btn.addEventListener('click', function () {
+  getCountryData('nepal');
+});
